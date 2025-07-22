2c540d87471dd403cbc1b28496979fa44e12d3a9
Max Petschack <18609293+Midataur@users.noreply.github.com>
2025-07-22 11:00:42 +1000

    this time for sure

--- fixmymess.py
+++ fixmymess.py
@@ -18,7 +18,7 @@ for data_type in ["train", "val", "test"]:
 
     # replace the malformed tokens
     print("Fixing...")
-    dataset.card_choices[dataset.state_cat == MALFORMED] = CORRECT
+    dataset.card_choices[dataset.card_choices == MALFORMED] = CORRECT
 
     print("Saving...")
     # save the dataset
